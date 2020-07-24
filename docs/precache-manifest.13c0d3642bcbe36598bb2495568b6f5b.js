ece41bbdbe59d02efeeb81326e9d7925d6202a78
Bryan Camilo <camirodbryan23@gmail.com>
2020-07-23 23:18:54 -0500

    docs agregado

---
+++ docs/precache-manifest.13c0d3642bcbe36598bb2495568b6f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0416c283fd47cc08dba4ad901f661aa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064a3b98f79bc1bcdce0",
+    "url": "/static/css/main.a5e12778.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064a3b98f79bc1bcdce0",
+    "url": "/static/js/main.db4fd481.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
